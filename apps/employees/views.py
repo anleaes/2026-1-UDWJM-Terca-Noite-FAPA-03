27cafa4af5cacbd9f243b41f4aa5333c8be0bd6a
vinicius borba <viniboal28@gmail.com>
2026-05-19 23:15:21 -0300

    feat add delete and edit employees

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -6,7 +6,6 @@ from .models import Employee
 
 def add_employee(request):
     template_name = 'employees/add_employee.html'
-    context = {}
     if request.method == 'POST':
         form = EmployeeForm(request.POST)
         if form.is_valid():
@@ -14,9 +13,9 @@ def add_employee(request):
             f.save()
             form.save_m2m()
             return redirect('employees:list_employees')
-    form = EmployeeForm()
-    context['form'] = form
-    return render(request, template_name, context)
+    else:
+        form = EmployeeForm()
+    return render(request, template_name, {'form': form})
 
 
 def list_employees(request):
@@ -30,16 +29,15 @@ def list_employees(request):
 
 def edit_employee(request, id_employee):
     template_name = 'employees/add_employee.html'
-    context = {}
     employee = get_object_or_404(Employee, id=id_employee)
     if request.method == 'POST':
         form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
             return redirect('employees:list_employees')
-    form = EmployeeForm(instance=employee)
-    context['form'] = form
-    return render(request, template_name, context)
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, template_name, {'form': form})
 
 
 def delete_employee(request, id_employee):
