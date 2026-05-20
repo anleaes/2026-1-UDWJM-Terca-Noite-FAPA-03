f7e7ce853415acca17ffbb702ecbc9dde31f3469
vinicius borba <viniboal28@gmail.com>
2026-05-19 23:00:52 -0300

    feat add employees urls

--- apps/employees/urls.py
+++ apps/employees/urls.py
@@ -6,4 +6,6 @@ app_name = 'employees'
 urlpatterns = [
     path('add/', views.add_employee, name='add_employee'),
     path('list/', views.list_employees, name='list_employees'),
+    path('edit/<int:id_employee>/', views.edit_employee, name='edit_employee'),
+    path('delete/<int:id_employee>/', views.delete_employee, name='delete_employee'),
 ]
