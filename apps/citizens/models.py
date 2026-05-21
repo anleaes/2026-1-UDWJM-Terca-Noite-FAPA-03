2e48e5dc2e8d108eab5447e1d48f877cdcb593bb
vinicius borba <viniboal28@gmail.com>
2026-05-21 15:47:33 -0300

    feat add citizens initial app

---
+++ apps/citizens/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from employees.models import Person
+
+# Create your models here.
+class Citizen(Person):
+    registration_number = models.CharField('Número de Cadastro', max_length=50)
+    neighborhood_association = models.CharField('Associação de Bairro', max_length=200)
+
+    class Meta:
+        verbose_name = 'Cidadão'
+        verbose_name_plural = 'Cidadãos'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
