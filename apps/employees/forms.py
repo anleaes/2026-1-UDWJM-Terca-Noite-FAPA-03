467a6798800a208841d2be61e0f0cfc16bba7096
vinicius borba <viniboal28@gmail.com>
2026-05-25 17:35:17 -0300

    fix employees forms

--- apps/employees/forms.py
+++ apps/employees/forms.py
@@ -9,7 +9,7 @@ class EmployeeForm(forms.ModelForm):
             format='%Y-%m-%d',
         ),
         input_formats=['%Y-%m-%d'],
-        label='Data de contratação',
+        label='Data de Admissão',
     )
 
     class Meta:
@@ -18,11 +18,12 @@ class EmployeeForm(forms.ModelForm):
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'national_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'national_id': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '000.000.000-00'}),
             'address': forms.TextInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '(00) 00000-0000'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'salary': forms.NumberInput(attrs={'class': 'form-control'}),
-            'position': forms.TextInput(attrs={'class': 'form-control'}),
+            'salary': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+            'position': forms.Select(attrs={'class': 'form-select'}),
             'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'company': forms.Select(attrs={'class': 'form-select'}),
         }
