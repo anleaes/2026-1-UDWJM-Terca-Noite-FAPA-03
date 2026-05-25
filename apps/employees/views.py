7ecabc98bf9f5d71197cd47f2f6527eb9d59aa97
vinicius borba <viniboal28@gmail.com>
2026-05-25 17:35:24 -0300

    fix employees views

--- apps/employees/views.py
+++ apps/employees/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
 from .forms import EmployeeForm
 from .models import Employee
 
@@ -11,16 +12,16 @@ def add_employee(request):
             f = form.save(commit=False)
             f.save()
             form.save_m2m()
-            return redirect('employees:list_employee')
+            return redirect('employees:list_employees')
     else:
         form = EmployeeForm()
     return render(request, template_name, {'form': form})
 
 
-def list_employee(request):
-    template_name = 'employees/list_employee.html'
-    itens = Employee.objects.filter()
-    context = {'itens': itens}
+def list_employees(request):
+    template_name = 'employees/list_employees.html'
+    employees = Employee.objects.filter()
+    context = {'employees': employees}
     return render(request, template_name, context)
 
 
@@ -31,7 +32,7 @@ def edit_employee(request, id_employee):
         form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
-            return redirect('employees:list_employee')
+            return redirect('employees:list_employees')
     else:
         form = EmployeeForm(instance=employee)
     return render(request, template_name, {'form': form})
@@ -40,4 +41,4 @@ def edit_employee(request, id_employee):
 def delete_employee(request, id_employee):
     employee = get_object_or_404(Employee, id=id_employee)
     employee.delete()
-    return redirect('employees:list_employee')
+    return redirect('employees:list_employees')
