3bde181ab71bd16518d5e6c478672d2e5b2c3bb7
Eduardo <almeidabedu@gmail.com>
2026-06-01 16:35:52 -0300

    fix other aplications

--- apps/contracts/urls.py
+++ apps/contracts/urls.py
@@ -8,9 +8,9 @@ router = routers.SimpleRouter()
 router.register('', views.ContractViewSet, basename='contracts')
 
 urlpatterns = [
-    path('add/', views.add_contract, name='add_contract'),
-    path('list/', views.list_contracts, name='list_contracts'),
-    path('edit/<int:id_contract>/', views.edit_contract, name='edit_contract'),
-    path('delete/<int:id_contract>/', views.delete_contract, name='delete_contract'),
+    path('add/<int:construction_id>/', views.add_contract, name='add_contract'),
+    path('list/<int:construction_id>/', views.list_contracts, name='list_contracts'),
+    path('edit/<int:construction_id>/<int:id_contract>/', views.edit_contract, name='edit_contract'),
+    path('delete/<int:construction_id>/<int:id_contract>/', views.delete_contract, name='delete_contract'),
     path('', include(router.urls)),
 ]
