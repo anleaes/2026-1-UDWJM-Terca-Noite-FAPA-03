0e8c1ef5b7873ebd9a222c2f2e2f52aaa1415781
FelipeSantos00 <160798488+FelipeSantos00@users.noreply.github.com>
2026-06-01 12:31:02 -0300

    feat add audit_reports/serializer

--- apps/audit_reports/urls.py
+++ apps/audit_reports/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 app_name = 'audit_reports'
+
+router = routers.SimpleRouter()
+router.register('', views.AuditReportViewSet, basename='audit_reports')
 
 urlpatterns = [
     path('add/', views.add_audit_report, name='add_audit_report'),
     path('list/', views.list_audit_report, name='list_audit_report'),
     path('edit/<int:id_audit_report>/', views.edit_audit_report, name='edit_audit_report'),
     path('delete/<int:id_audit_report>/', views.delete_audit_report, name='delete_audit_report'),
+    path('', include(router.urls)),
 ]
